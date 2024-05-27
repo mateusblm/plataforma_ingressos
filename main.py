80155b378c63e890136ea1c2ae71c0aef14c8e6e
Mateus Burlamaqui Moreira <mateus.burlamaqui@pucpr.edu.br>
2024-05-27 10:49:33 -0300

    Adicionando as entrys para inserir usuario e senha

--- main.py
+++ main.py
@@ -10,7 +10,17 @@ if __name__ == '__main__':
     btn1 = CTkButton(master=root, text="Login", corner_radius=32, font=font1, width=100)
     btn2 = CTkButton(master=root, text="Sign up", corner_radius=32, font=font1, width=100)
     label1 = CTkLabel(master=root, font=font1, text="Usuario")
-    label1.place(relx=0.25, rely=0.3, anchor="center")
+    label2 = CTkLabel(master=root, font=font1, text="Senha")
+
+    entry_user = CTkEntry(root, placeholder_text="Digite o usuario")
+    entry_user.place(relx=0.50, rely=0.35, anchor="center")
+    entry_pass = CTkEntry(root, placeholder_text="Digite a senha", show="*")
+    entry_pass.place(relx=0.50, rely=0.55, anchor="center")
+
+
+    label1.place(relx=0.50, rely=0.25, anchor="center")
+    label2.place(relx=0.50, rely=0.45, anchor="center")
+
     btn1.place(relx=0.65, rely=0.7, anchor="center")
     btn2.place(relx=0.3, rely=0.7, anchor="center")
 
